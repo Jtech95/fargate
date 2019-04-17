bc4fd393e7b7c6c248dde727a4cd8aadc042442b
EC2 Default User <ec2-user@ip-172-31-92-127.ec2.internal>
2019-04-17 18:20:56 +0000

    added everything

---
+++ run_task.py
@@ -0,0 +1,18 @@
+import boto3
+def handler(event,context):
+  client = boto3.client('ecs')
+  response = client.run_task(
+  cluster='FG-cluster', # name of the cluster
+  launchType = 'FARGATE',
+  taskDefinition='FG-test:2', # replace with your task definition name and revision
+  count = 1,
+  platformVersion='LATEST',
+  networkConfiguration={
+        'awsvpcConfiguration': {
+            'subnets': [
+                'subnet-25daba0b', # replace with your public subnet or a private with NAT
+            ],
+            'assignPublicIp': 'ENABLED'
+        }
+    })
+  return str(response)
